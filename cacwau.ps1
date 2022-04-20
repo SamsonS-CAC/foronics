bd5e4a58f085b3b6971aec90ff46a33922cd061d
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2022-04-20 14:18:44 -0700

    Update cacwau.ps1

--- cacwau.ps1
+++ cacwau.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "SilentlyContinue"
 $d = "c:\win10exe"
 mkdir -p $d
 $ComObj = New-Object System.Net.WebClient
-$exedl = “https://go.microsoft.com/fwlink/?LinkID=799445"
-$exe = “$($d)\Win10Upgrade.exe”
+$exedl = "https://go.microsoft.com/fwlink/?LinkID=799445"
+$exe = "$($d)\Win10Upgrade.exe"
 $ComObj.DownloadFile($exedl,$exe)
-Start-Process -FilePath $exe -ArgumentList “/quietinstall /skipeula /auto upgrade /dynamicupdate enable /copylogs $d”
+Start-Process -FilePath $exe -ArgumentList "/quietinstall /skipeula /auto upgrade /dynamicupdate enable /copylogs $d"
