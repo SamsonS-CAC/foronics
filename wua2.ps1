fa1bdb209128afcde175008e16e73f6f78ae3957
RobKCAC <104100331+RobKCAC@users.noreply.github.com>
2022-04-20 13:38:56 -0700

    Create wua2.ps1

---
+++ wua2.ps1
@@ -0,0 +1,1 @@
+Start-Process -FilePath C:\Program Files (x86)\WindowsInstallationAssistant\Windows10UpgraderApp.exe -ArgumentList “/quietinstall /skipeula /auto upgrade /dynamicupdate enable /copylogs c:\”
